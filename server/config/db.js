8fab7a8e41302b49dcd2af20d413d5d1104dd80a
divyank0203 <divyankh444@gmail.com>
2026-01-09 16:55:02 +0530

    mongodb connected

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,17 @@
 const mongoose = require("mongoose");
-
+const env = require('dotenv');
+env.config();
 const MONGO_URI = process.env.MONGO_URI;
+
+const connectDB = async function (){
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log("MongoDB connected successfully");
+    }
+    catch(error){
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
--- server/index.js
+++ server/index.js
@@ -5,9 +5,15 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const env = require('dotenv');
 
+const connectDB = require('./config/db');
+
+// Connect to MongoDB
+connectDB();
+
+
 env.config();
 const PORT = process.env.PORT;
-const MONGO_URI = process.env.MONGO_URI;
+// const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors());
 app.use(express.json());
