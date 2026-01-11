a384d00b203a2b122954965af9b51d0dde8eff5d
divyank0203 <divyankh444@gmail.com>
2026-01-12 00:13:02 +0530

    bcrypt.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,17 +1,35 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 import env from 'dotenv';
 
 env.config();
 const MONGO_URI = process.env.MONGO_URI;
 
-const UserSchema = new Schema({
+const UserSchema = new mongoose.Schema({
     firstname: { type: String, required: true },
     lastname: { type: String, required: false },
     email: { type: String, unique: true},
     password: { type: String, required: true, minlength: 6, select: false },
     createdAt: { type: Date, default: Date.now }
 
+}, 
+{ timestamps: true });
+
+
+// Pre-save hook to hash password before saving
+UserSchema.pre('save', async function(next){
+    if(!this.isModified('password')){
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 })
+
+// Method to compare entered password with hashed password
+UserSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+}
 
 const User = mongoose.model("User", UserSchema);
 
