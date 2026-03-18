188f7d6f799dd5831b9c9969feaa17478f36f5d2
divyank0203 <divyankh444@gmail.com>
2026-03-18 18:45:14 +0530

    login route created

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -45,4 +45,51 @@ router.post('/register', async (req, res, next) => {
     }
 })
 
+//Login
+router.post('/login', async (req, res, next) => {
+    try{
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({
+                status: false,
+                message: "Email and Password are required"
+            })
+        }
+       // const existingUser = await User.findOne({ email }).select('+password');
+        const existingUser = await User.findOne({ email }).select('+password');
+        if(!existingUser){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email or password",
+            })
+        }
+        const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+        if(!isPasswordValid){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email or password",
+            })
+        }
+
+        const token = jwt.sign({
+            id: existingUser._id},
+        JWT_SECRET,
+    {
+        expiresIn: '4d'
+    })
+        return res.status(200).json({
+            success: true,
+            message: "Login successful",
+            token
+        })
+    }
+    catch(error){
+        console.error("Error during login: ", error);
+        return res.status(500).json({
+            success: false,
+            message: "Server error during login"
+        })
+    }
+})
+
 export default router;
